841bce4a8c65d769c0230c6e97c5521a2cd279fd
asreeve <asreeve@maine.edu>
2020-10-28 15:41:22 -0400

    add water resources slideshow

---
+++ ers102_slides/19_water_resources/quiz/water_res.js
@@ -0,0 +1,70 @@
+water_res_quiz={
+	"info": {
+		"name":    "",
+		"main":    "Quiz Time! Test your knowledge. Use 'ctrl +' or 'ctrl -' key combination to shrink/expand text to fit question on screen",
+	},
+	"questions": [
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What reservoirs contain most of the world's *fresh* water?",
+			"a": [
+				{"option": "groundwater and glaciers", "correct": true},
+				{"option": "soil moisture and wetlands", "incorrect": false},
+                {"option": "lakes and rivers", "incorrect": false},
+				{"option": "plants and animals (biomass)", "incorrect": false},
+                {"option": "the atmosphere and soil vapor", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "Which of the following is an example of consumptive (or offstream)  use of water?",
+			"a": [
+				{"option": "navigation through waterways", "incorrect": false},
+				{"option": "municipal water use (e.g. drinking water)", "correct": true},
+                {"option": "recreational boating and swimming", "incorrect": false},
+				{"option": "hydropower production", "incorrect": false},
+                {"option": "protection of aquatic habitat", "incorrect": false},
+			],
+			"correct": "Right! Consumptive indicates the water is removed and used, and not directy returned to a system. The other listed examples do not remove water (at least for very long) from a system",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What are the biggest total users of water in the USA?",
+			"a": [
+				{"option": "livestock and mining", "incorrect": false},
+				{"option": "aquiculture and fish hatcheries", "incorrect": false},
+                {"option": "public (municipal) and domestic water supply", "incorrect": false},
+				{"option": "bottled water, beer, and soft drink production", "incorrect": false},
+                {"option": "thermoelectric power plants and irrigation", "correct": true},
+			],
+			"correct": "Right :-)",
+			"incorrect": "Wrong"
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "Where does most rainwater get allocated to (or lost) in a drainage basin?",
+			"a": [
+				{"option": "the majority of rainwater is evaporated", "correct": true},
+				{"option": "the majority of rainwater is stored in soils", "incorrect": false},
+                {"option": "the majority of rainwater flows into streams and rivers", "incorrect": false},
+				{"option": "the majority of rainwater infiltrates into the  groundwater", "incorrect": false},
+                {"option": "the majority of rainwater is consumed by plants and animals", "incorrect": false},
+			],
+			"correct": "Right, perhaps surprisingly, the larges proportion of water leaves a drainage basin by evaporation (and transporation), putting it back into the atmosphere",
+			"incorrect": "incorrect"
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What is the average residence time for water in glaciers?",
+			"a": [
+				{"option": "7 to 14 days", "incorrect": false},
+				{"option": "2 to 6 months", "incorrect": false},
+                {"option": "1 to 5 years", "incorrect": false},
+				{"option": "20 to 100 years", "correct": true},
+                {"option": "100,000 to 200,000 years", "incorrect": false},
+			],
+			"correct": "Right, Glaciers store water for relatively long periods of time. An entire glacial period might last 100,000 years",
+			"incorrect": "Wrong :-("
+		},
+	]	
+}
+
+
