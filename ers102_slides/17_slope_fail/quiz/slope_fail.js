09ac6436068ad28ca2725130b5f6e27bd75c815b
asreeve <asreeve@maine.edu>
2020-10-28 00:09:10 -0400

    added subsidence slideshow, quiz added to slope failure slides

---
+++ ers102_slides/17_slope_fail/quiz/slope_fail.js
@@ -0,0 +1,57 @@
+slope_fail_quiz={
+	"info": {
+		"name":    "",
+		"main":    "Time for a quiz! Test your knowledge. Use 'ctrl +' or 'ctrl -' key combination to shrink/expand text to fit question on screen",
+	},
+	"questions": [
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What is the primary driving force for the movement of earth materials?",
+			"a": [
+				{"option": " Gravity", "correct": true},
+				{"option": " Heat", "incorrect": false},
+                {"option": " Glaciers", "incorrect": false},
+				{"option": " Organisms", "incorrect": false},
+                {"option": " Wind", "incorrect": false},
+			],
+			"correct": "Right!, without gravity there would be no slope failures.",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "In slope failures, what distinguished a slide from a flow?",
+			"a": [
+				{"option": "water is always involved in flows, while slides are commonly unrelated to water", "incorrect": false},
+				{"option": "slides occur on steeper banks than flows", "incorrect": false},
+                {"option": "slides are commonly found on playgrounds, whereas flows occur in banks", "incorrect": false},
+				{"option": "slides are influenced by human activity, while flows are commonly natural events", "incorrect": false},
+                {"option": "there is mixing inside a flow, whereas a slide occurs as a coherent unit of material", "correct": true},
+			],
+			"correct": "Right! Slides (and slumps) move as a relatively undisturbed block whereas a flow is a chaotic mixture of flowing materials",
+			"incorrect": "No, try again."
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "When considering slope failure, does water increase or decrease the resisting force of a material?",
+			"a": [
+				{"option": "large amounts of water increase the resisting force and small amounts of water decrease the resisting force", "incorrect": false},
+				{"option": "small amounts of water increase the resisting force and large amounts of water decrease the resisting force", "correct": true},
+				{"option": "water increases the resisting force", "incorrect": false},
+				{"option": "water has no effect on slope failure", "incorrect": false},
+                {"option": "water decreases the resisting force", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "Which of the following actions would increase the stability of a slope and decrease the likelihood of a slope failure?",
+			"a": [
+				{"option": " add weight at the top of the slope", "incorrect": false},
+				{"option": " heavily irrigate the slope", "incorrect": false},
+                {"option": " landscaping the top of the slope to reduce runoff and increase ponding of water", "incorrect": false},
+				{"option": " excavate to make the sloping surface steeper", "incorrect": false},
+                {"option": " adding pipes to drain water from the geologic material under the slope", "correct": true},
+			],
+			"correct": "Muy Bien, removing excess water that lubricates sediments, adds mass (driving force_, and may increase pore pressure will stabilize a slope.",
+			"incorrect": "Wrong :-("
+		},
+	]	
+}
+
