198364f6cb4e7ba207b8458b8f37b4cf4e4b9929
asreeve <asreeve@maine.edu>
2020-12-01 12:30:01 -0500

    updated climate change slideshows

---
+++ ers102_slides/24_climate_chng_p1/quiz/atmosphere.js
@@ -0,0 +1,116 @@
+atmosphere_quiz={
+	"info": {
+		"name":    "",
+		"main":    "Test your knowledge with a quiz. Note that this covers both the groundwater (videos) and water chemistry material. Use 'ctrl +' or 'ctrl -' key combination to shrink/expand text to fit question on screen",
+	},
+	"questions": [
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What are the two primary compounds generated from burning fossil fuels that produce strong acids found in acid rain?",
+			"a": [
+				{"option": "$CO_2$ and $CH_4$", "incorrect": false},
+				{"option": "$HgO$ and $Fe_2O_3$", "incorrect": false},
+                {"option": "$H_2O$ and $O_2$", "incorrect": false},
+				{"option": "$CH_2O$ and $CaCO_3$", "incorrect": false},
+                {"option": "$NO_X$ and $SO_X$", "correct": true},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "Which chemical group destroys ozone in the stratosphere, resulting the in ozone 'hole'?",
+			"a": [
+				{"option": "$SO_x$ and $NO_x$", "incorrect": false},
+				{"option": "hydrocarbons", "incorrect": false},
+                {"option": "$CO_2$", "incorrect": false},
+				{"option": "Chlorofluorocarbons", "correct": true},
+                {"option": "MTBE", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "How does the 'greenhouse effect' work?",
+			"a": [
+				{"option": "long wave infrared shifts its frequency in the presence of greenhouse gasses, that increases their ability to heat gasses", "incorrect": false},
+				{"option": "short wave UV radiation is absorbed more by surfaces in the presence of greenhouse gasses, enhancing black body radiation", "incorrect": false},
+                {"option": "long wave infrared radiation is absorbed by certain gasses and re-radiated back into the atmosphere, trapping heat.", "correct": true},
+				{"option": "greenhouse gasses are excellent insulators, preventing heat from being transported into space", "incorrect": false},
+                {"option": "greenhouse gasses undergo spontaneous fission, releasing heat during nuclear decay", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What is black body radiation, in the context of the Earth's heat budget?",
+			"a": [
+				{"option": "enhanced heating of the earth due to combustion of coal and oil (black fuels)", "incorrect": false},
+				{"option": "heat that accumulates over oil spills and other 'black bodies' due to enhanced absorption of long wave radiation", "incorrect": false},
+                {"option": "absorption of short wave radiation by the Earth's surfaces and the subsequent emission of long wave radiation", "correct": true},
+				{"option": "the radioactive decay of 'black' elements, releasing heat into the atmosphere", "incorrect": false},
+                {"option": "the enhanced reflection of light from surfaces due to their reduced albedo", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "How do scientists determine past climates (before widespread human records)?",
+			"a": [
+				{"option": "by using satellites that can measure old light, allowing scientists to see the past", "incorrect": false},
+				{"option": "by measuring parameters 'recorded' in geologic and biological materials that have accumulated over long periods of time", "correct": true},
+                {"option": "by carefully examining the genetic code of organisms like sharks and turtles, that likely where present in the past", "incorrect": false},
+				{"option": "through the use of 'rail guns' that allow scientist to send weather instruments back in time", "incorrect": false},
+                {"option": "careful separation of old water from oceans and measuring its temperature and chemistry", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What gas in the atmosphere is the most important 'greenhouse gas', accounting for most of the warming associated with these gasses.",
+			"a": [
+				{"option": " water vapor ($H_2O$)", "correct": true},
+				{"option": " nitrous oxide ($N_2O$)", "incorrect": false},
+                {"option": " methane ($CH_4$)", "incorrect": false},
+				{"option": " carbon dioxide ($CO_2$)", "incorrect": false},
+                {"option": " chloroflourocarbons (CFC's)", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "How are the boundaries between layers in the atmosphere (Troposphere, Stratosphere. etc.) determined or defined?",
+			"a": [
+				{"option": " Different concentrations of gasses (shift from Oxygen-rich to $CO_2$-rich)", "incorrect": false},
+				{"option": " Maximum altitude that butterflies, birds and other flying organisms can reach", "incorrect": false},
+                {"option": " Temperature gradients (e.g. cooling vs. warming with altitude)", "correct": true},
+				{"option": " Maximum altitude that measurement devices, such as weather balloons, can reach", "incorrect": false},
+                {"option": " Differential absorption of solar radiation", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "How do hurricanes form?",
+			"a": [
+				{"option": " old ocean water drives as downward flow of air that is pushed outward from this high pressure zone", "incorrect": false},
+				{"option": " easterly winds hit a warm weather front that drives vortex formation", "incorrect": false},
+                {"option": " warm ocean water drives an updraft in the atmosphere, that pulls air in to this low pressure zone", "correct": true},
+				{"option": " rapidly flying sea gulls chase each other in a big circle, that spans hurricane strength winds and liquids falling from the sky", "incorrect": false},
+                {"option": " short wave radiation, reflected from the ocean surface (when bowl shaped), pushes air in a vortex", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What are the consequences of an El Ni\~no event?",
+			"a": [
+				{"option": " smog forms in eastern US cities (New York, Boston).", "incorrect": false},
+				{"option": " reduced atmospheric circulation and a warming east Pacific Ocean causes droughts in Australia and increased precipitation in the western portion of the Americas.", "correct": true},
+                {"option": " heavy snowfall, very cold temperatures, and/or dangerous wind speeds occur in the Midwest (USA).", "incorrect": false},
+				{"option": " increased melting of the ice sheets near the north pole, impacting the native populations.", "incorrect": false},
+                {"option": " important environmental legislation is unable to pass through the U.S. Congress.", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+	]
+}
---
+++ ers102_slides/24_climate_chng_p1/quiz/climate_chng.js
@@ -0,0 +1,57 @@
+climate_chng_quiz={
+	"info": {
+		"name":    "",
+		"main":    "Test your knowledge with a quiz. Note that this covers both the groundwater (videos) and water chemistry material. Use 'ctrl +' or 'ctrl -' key combination to shrink/expand text to fit question on screen",
+	},
+	"questions": [	
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "How do ocean currents influence the global and local climate?",
+			"a": [
+				{"option": " Ocean currents transport heat, absorbed in ocean water, from warm to cool parts of the earth.", "correct": true},
+				{"option": " Ocean currents transport sediments to the ocean surface that consume oxygen during weathering reactions and release $CO_2$ to the atmosphere", "incorrect": false},
+                {"option": " Ocean currents have little impact on the climate.", "incorrect": false},
+				{"option": " Ocean currents transport ozone, that is released to coastal areas, from the polar regions.", "incorrect": false},
+                {"option": " Ocean currents transport algae, from deep in the ocean to shallow waters, that consume carbon dioxide and release oxygen to the atmosphere.", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What is the primary cause behind current ocean acidification?",
+			"a": [
+				{"option": " Release of toxic chemical solvents into the oceans, like trichloroethylene, results in near-shore acidification", "incorrect": false},
+				{"option": " Increased groundwater discharge to the oceans drive acidification", "incorrect": false},
+                {"option": " Human pollution, including acid rain, is acidifying the oceans", "incorrect": false},
+				{"option": " Increasing atmospheric CO2 is dissolved in water producing a weak acid", "correct": true},
+                {"option": " Algae growth in oceans, stimulated by nutrients, produce oxygen that drives increasing acidification", "incorrect": false},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "How do scientist estimate past greenhouse gas concentrations many hundreds of years ago?",
+			"a": [
+				{"option": " By measuring the chemical makeup of deep ocean water.", "incorrect": false},
+				{"option": " By measuring the chemical composition of metamorphic rocks.", "incorrect": false},
+                {"option": " Through analysis of gases transported into the deep earth through subduction.", "incorrect": false},
+				{"option": " By looking at plant fossils used ancient CFC's for photosynthesis.", "incorrect": false},
+                {"option": " By measuring the concentrations of gases trapped in bubbles in ice cores collected from glaciers.", "correct": true},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "When presenting a scientific (or other) argument, what is a 'false dichotomy'?",
+			"a": [
+				{"option": " excluding data that does not support your argument", "incorrect": false},
+				{"option": " punching someone in the nose when they say something you don't like", "incorrect": false},
+                {"option": " cleverly packaging logic that is not correct", "incorrect": false},
+				{"option": " presenting a distraction that shifts attention", "incorrect": false},
+                {"option": " incorrectly limiting choices", "correct": true},
+			],
+			"correct": "Right!",
+			"incorrect": "Wrong :-("
+		},
+	]
+}
+
