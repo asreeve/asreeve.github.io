1642a005e9252829908e785e01dda21bde524caf
asreeve <asreeve@maine.edu>
2020-12-07 20:30:42 -0500

    add energy slides

---
+++ ers102_slides/25_energy/quiz/energy.js
@@ -0,0 +1,44 @@
+energy_quiz={
+	"info": {
+		"name":    "",
+		"main":    "Test your knowledge with a quiz. Note that this covers both the groundwater (videos) and water chemistry material. Use 'ctrl +' or 'ctrl -' key combination to shrink/expand text to fit question on screen",
+	},
+	"questions": [	
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What is the \textbf{ultimate} source of most energy resources used by humans?",
+			"a": [
+				{"option": "geothermal heat from a cooling earth", "incorrect": false},
+				{"option": "gravity for the moon", "incorrect": false},
+                                {"option": "light from the sun", "correct": true},
+				{"option": "radioactive decay in the Earth", "incorrect": false},
+                                {"option": "high fructose corn syrup", "incorrect": false},
+			],
+			"correct": "Right! Energy from the sun drive photosynthesis, wind, and evaporation",
+			"incorrect": "Guess again!"
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What resource currently represents the largest potential non-renewable energy resource on Earth?",
+			"a": [
+				{"option": "Oil", "incorrect": false},
+				{"option": "Peat", "incorrect": false},
+                                {"option": "Natural Gas", "incorrect": false},
+				{"option": "Coal", "incorrect": false},
+                                {"option": "Uranium", "correct": true},
+			],
+			"correct": "Right! Using Uranium to supply nuclear energy would last into the forseeable future",
+			"incorrect": "Wrong :-("
+		},
+		{ // Question 1 - Multiple Choice, Single True Answer
+			"q": "What basic process is behind the concentration of uranium in mineral deposits (e.g. roll fronts)?",
+			"a": [
+				{"option": " Decreasing the pH of water to a more basic (or less acidic) conditions", "incorrect": false},
+				{"option": " Evaporation the concentrates solutes in water", "incorrect": false},
+                                {"option": " Fusion of other elements to form uranium in sedimentary rocks", "incorrect": false},
+				{"option": " A change in the geochemistry of water from oxidizing to reducing conditions", "correct": true},
+                                {"option": " Density seperation of the dissolved uranium from the water", "incorrect": false},
+			],
+			"correct": "Right! Uranium is becomes less soluble in reducing environments, and the change in this conditions drives precipitation or uranium oxides",
+			"incorrect": "Lo siento. Intente de nuevo!"
+		},
+	]
+}
