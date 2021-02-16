42b8bdf401dd9290089ed429b75569ee368d4d7e
asreeve <asreeve@maine.edu>
2021-02-15 21:14:46 -0500

    add python exception slideshow, fix indentation error in python class page

---
+++ slideshows/ers420/08_except/08_except.rst
@@ -0,0 +1,17 @@
+
+
+
+Section
+-------
+
+Introduction to Exceptions
+--------------------------
+
+Using if and try-except Clauses for errors
+------------------------------------------
+
+Common Exception
+----------------
+
+Catching and Raising exceptions
+-------------------------------
---
+++ slideshows/ers420/08_except/add_reveal_plugins.py
@@ -0,0 +1,34 @@
+
+infile = '08_except.html'
+outfile = '08_except_v4.html'
+
+flag = False
+
+old_strings=[
+    '<script src="../reveal.js/dist/reveal.js"></script>\n',
+    'plugins: [RevealHighlight],\n',]
+
+new_strings=[
+    '''
+    <script src="../reveal.js/dist/reveal.js"></script>
+    <script src="../reveal.js/plugin/audio-slideshow/plugin.js"></script>
+    <script src="../reveal.js/plugin/menu/menu.js"></script>
+    ''',
+    "plugins: [ RevealAudioSlideshow, RevealMenu, RevealHighlight],\n"
+    ]
+
+with open(infile) as fo:
+    textfile=fo.readlines()
+    for i,ln in enumerate(textfile):
+        if (flag==False):
+            if ln == old_strings[0]:
+                textfile[i]=ln.replace(ln,new_strings[0])
+                flag=True
+        else:
+            if ln == old_strings[1]:
+                textfile[i]=ln.replace(ln,new_strings[1])
+            
+with open(outfile, 'wt') as fo:
+    for line in textfile:
+        fo.write(line)
+    
